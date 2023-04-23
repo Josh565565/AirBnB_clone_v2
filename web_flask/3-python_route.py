d772b39e6c3a5b7a1063af241f2ed6fb17e2eaa8
Okwor Okechukwu Joshua <joshuaokwor565@icloud.com>
2023-04-23 07:53:22 -0700

    task3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -32,6 +32,7 @@ def c(text):
     return "C {}".format(text)
 
 
+@app.route(("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def p(text="is cool"):
     text = text.replace("_", " ")
