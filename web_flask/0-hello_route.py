30052b966a1db3db69ba02781dba0fcf46043286
Okwor Okechukwu Joshua <joshuaokwor565@icloud.com>
2023-04-23 06:50:57 -0700

    task0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
-"""
-A flask web app that:
-    listening on 0.0.0.0, port 5000
-    Routes:/ 
-    display “Hello HBNB!”
-    uses the option strict_slashes=False in your route definition
+"""Starts a Flask web application.
 
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'
 """
-
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/", strict_slashes=False)
 
-def hello_route():
-    """This function displays Hello HBNB!"""
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
