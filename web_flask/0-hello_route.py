ebe5d7cbc4a7e4e9d300b50ebcf644c1bb1e0c28
Okwor Okechukwu Joshua <joshuaokwor565@icloud.com>
2023-04-23 06:40:30 -0700

    task0

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+A flask web app that:
+    listening on 0.0.0.0, port 5000
+    Routes:/ 
+    display “Hello HBNB!”
+    uses the option strict_slashes=False in your route definition
+
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+
+def hello_hbnb():
+    """This function displays Hello HBNB!"""
+    return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port 5000)
