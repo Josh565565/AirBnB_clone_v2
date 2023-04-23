8911e978c8fece6e0c81e7eccc4ea03131bfeaec
Okwor Okechukwu Joshua <joshuaokwor565@icloud.com>
2023-04-23 07:36:22 -0700

    task2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """Starts a Flask web application.
+
 The application listens on 0.0.0.0, port 5000.
 Routes:
-    /: Displays 'Hello HBNB!'
+    /: Displays 'Hello HBNB!'.
+    /hbnb: Displays 'HBNB'.
+    /c/<text>: Displays 'C' followed by the value of <text>.
 """
 from flask import Flask
 
@@ -11,21 +14,21 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """This function displays Hello HBNB!"""
+    """Displays 'Hello HBNB!'."""
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """This function displays HBNB"""
+    """Displays 'HBNB'."""
     return "HBNB"
-
 
 
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
-    """display “C ” followed by the value of the text variable (replace underscore _ symbols with a space"""
-    return "C {}".format(text.replace("_",( ))
+    """Displays 'C' followed by the value of <text>."""
+    text = text.replace("_", " ")
+    return "C {}".format(text)
 
 
 if __name__ == "__main__":
