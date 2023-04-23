39c2c6c02e934b1e4ba8ac871b80edf822279af0
Okwor Okechukwu Joshua <joshuaokwor565@icloud.com>
2023-04-23 06:47:49 -0700

    task0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -12,11 +12,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 
 def hello_route():
     """This function displays Hello HBNB!"""
-    return 'Hello HBNB!'
+    return "Hello HBNB!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
