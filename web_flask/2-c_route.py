b9b66ac55263146e4aa9845a7c34fcf2c8bf2dd9
Okwor Okechukwu Joshua <joshuaokwor565@icloud.com>
2023-04-23 07:32:36 -0700

    task2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -26,7 +26,7 @@ def hbnb():
 def c(text):
     """display “C ” followed by the value of the text variable (replace underscore _ symbols with a space"""
     text = text.replace("_"," ")
-    return "c %s"% text
+    return "c {}".format(text)
 
 
 if __name__ == "__main__":
