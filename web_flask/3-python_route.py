75694a893a60d667bf10d86aa0144e4142a59aa8
Okwor Okechukwu Joshua <joshuaokwor565@icloud.com>
2023-04-23 08:02:52 -0700

    task3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -37,7 +37,7 @@ def c(text):
 
 @app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
-def pyth(text="is cool"):
+def py(text="is cool"):
     """Displays 'Python' followed by the value of <text>.
 
     Replaces any underscores in <text> with slashes.
