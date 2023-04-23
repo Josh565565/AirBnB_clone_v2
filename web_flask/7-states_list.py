994c8d401567893efd63d4e5f8a6fab1d152566a
Okwor Okechukwu Joshua <joshuaokwor565@icloud.com>
2023-04-23 09:59:10 -0700

    task7

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Starts a Flask web application.
+    /states_list: display a HTML page: (inside the tag BODY)
+"""
+from models import storage
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/states_list", strict_slashes=False)
+def list():
+    """display a HTML page: (inside the tag BODY)"""
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
+    storage.close()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
